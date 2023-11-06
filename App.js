5a70d21c7f769faa761d5e7678a47aa0bb05863b
antoine <mobileappandco@gmail.com>
2023-11-06 12:20:52 +0100

    fin leçon 96

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { StatusBar, StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React from "react";
 
 export default function App() {
-
-  const IsAndroid = Platform.OS === 'android';
-
-
-  console.log(StatusBar.currentHeight);
-
   return (
     <>
-      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+      <SafeAreaView style={styles.container}>
         <ExpoStatusBar />
-        <View style={{ padding: 16, backgroundColor: "green", flex: 1 / 20 }}>
+        <View style={styles.search}>
           <Text>Search</Text>
         </View>
-        <View style={{ padding: 16, flex: 19 / 20, backgroundColor: "blue" }}>
+        <View style={styles.list}>
           <Text>List</Text>
         </View>
       </SafeAreaView>
@@ -24,4 +19,18 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+    backgroundColor: "green",
+    flex: 1 / 20,
+  },
+  list: {
+    padding: 16,
+    flex: 19 / 20,
+    backgroundColor: "blue",
+  },
 });
